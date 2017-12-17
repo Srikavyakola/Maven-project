29b254dacfa81acec172d1ffb68335fafe327756
Kavya <Kavya@LAPTOP-AQPIHUQ7>
2017-12-17 17:05:06 +0530

    First commit

---
+++ src/test/java/com/selenium/project/MavenProject.java
@@ -0,0 +1,12 @@
+package com.selenium.project;
+
+import org.testng.annotations.Test;
+
+public class MavenProject {
+
+	@Test
+	public void testhello(){
+		
+		System.out.println("Welocme to Maven World!!");
+	}
+}
